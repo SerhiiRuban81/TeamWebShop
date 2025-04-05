bd5abd1f6bd9bdda095a48575a10859e52f995ad
serhii.ruban81 <serhii.ruban81@gmail.com>
2025-04-05 14:00:06 +0300

    Added `ShopLibrary` with basic classes.

---
+++ ShopLibrary/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopLibrary
+{
+    class Product
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; } = default!;
+
+        public string Description { get; set; } = default!;
+
+        public double Price { get; set; }
+
+        [ForeignKey(nameof(Brand))]
+        public int BrandId { get; set; }
+
+        public Brand Brand { get; set; } = default!;
+
+        public Category Category { get; set; } = default!;
+
+        public int CategoryId { get; set; }
+
+        public ICollection<ProductImage> ProductImages { get; set; } = default!;
+
+        // Added fields
+        public string? Color { get; set; }
+
+        public string? ProductModel { get; set; }
+    }
+}
---
+++ ShopLibrary/ProductImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopLibrary
+{
+    class ProductImage
+    {
+        public int Id { get; set; }
+
+        public byte[] ImageData { get; set; } = default!;
+
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; } = default!;
+    }
+}
