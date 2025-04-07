1f014e27178302d7035facf8aed41e1e4bb24b2e
PanDmytryk <dimacapny22@gmail.com>
2025-04-07 18:53:23 +0200

    Rename from 'DTO' to 'DTOs'

--- TeamWebShop/Models/DTOs/ProductDTO.cs
+++ TeamWebShop/Models/DTOs/ProductDTO.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TeamWebShop.Models.DTO
+namespace TeamWebShop.Models.DTOs
 {
     public class ProductDTO
     {
