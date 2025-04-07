9fb51f7db5c810f6a97dfd70ac62cf9a071289e7
Shers_um49 <Shers_um49@student.itstep.org>
2025-04-07 16:10:43 +0200

    Added wwwroot

---
+++ TeamWebShop/Data/ShopContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
+using System.Drawing.Drawing2D;
+using ShopLibrary;
+
+namespace TeamWebShop.Data
+{
+    public class ShopContext : IdentityDbContext<ShopUser>
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        { }
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductImage> Images { get; set; }
+    }
+}
--- TeamWebShop/Program.cs
+++ TeamWebShop/Program.cs
@@ -1,5 +1,26 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+//Addings
+string connStr = builder.Configuration.GetConnectionString("RozegroShopDb") ?? throw new InvalidCastException("Connection string not configured!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var app = builder.Build();
 
 app.MapControllerRoute(
