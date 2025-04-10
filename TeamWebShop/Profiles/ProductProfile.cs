e97f494bf76ecf8db1956e0133b8925f3480bea5
Shers_um49 <Shers_um49@student.itstep.org>
2025-04-10 18:32:56 +0200

    Added Images adding (still not all, work in progress)

---
+++ TeamWebShop/Profiles/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TeamWebShop.Models.DTOs.Products;
+
+namespace TeamWebShop.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<ProductProfile, ProductDTO>()
+                .ReverseMap();
+        }
+    }
+}
