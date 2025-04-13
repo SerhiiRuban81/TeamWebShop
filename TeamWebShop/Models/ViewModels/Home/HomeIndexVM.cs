ce4ce97afbc61da854ccc8f27086d8e69cbada7d
Shers_um49 <Shers_um49@student.itstep.org>
2025-04-13 17:27:36 +0200

    StartWorking on `Home` page

---
+++ TeamWebShop/Models/ViewModels/Home/HomeIndexVM.cs
@@ -0,0 +1,15 @@
+﻿using ShopLibrary;
+using System.Runtime.InteropServices;
+
+namespace TeamWebShop.Models.ViewModels.Home
+{
+    public class HomeIndexVM
+    {
+        public int CurrnetPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public IEnumerable<Product>? Products { get; set; }
+
+    }
+}
