ad62544af917aa89d870c30fd88b31ab765594dc
IT <you@gmail.com>
2025-04-16 21:04:40 +0300

    delete password

---
+++ TeamWebShop/Controllers/AdminController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeamWebShop.Models.DTOs.Admins;
+
+namespace TeamWebShop.Controllers
+{
+    public class AdminController : Controller
+    {
+        private static readonly List<AdminDTO> admins = new()
+        {
+            new AdminDTO { Id = 1, Email = "admin1@gmail.com", Permissions = new List<string> { "ManageUsers", "ViewReports" } },
+            new AdminDTO { Id = 2, Email = "admin2@gmail.com", Permissions = new List<string> { "ManageProducts" } }
+        };
+
+        public IActionResult Index()
+        {
+            return View(admins);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var admin = admins.FirstOrDefault(a => a.Id == id);
+            if (admin == null) return NotFound();
+            return View(admin);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(AdminDTO newAdmin)
+        {
+            if (ModelState.IsValid)
+            {
+                newAdmin.Id = admins.Max(a => a.Id) + 1;
+                admins.Add(newAdmin);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(newAdmin);
+        }
+        public IActionResult Edit(int id)
+        {
+            var admin = admins.FirstOrDefault(a => a.Id == id);
+            if (admin == null) return NotFound();
+            return View(admin);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, AdminDTO updatedAdmin)
+        {
+            if (!ModelState.IsValid) return View(updatedAdmin);
+
+            var existingAdmin = admins.FirstOrDefault(a => a.Id == id);
+            if (existingAdmin == null) return NotFound();
+
+            existingAdmin.Email = updatedAdmin.Email;
+            existingAdmin.Role = updatedAdmin.Role;
+            existingAdmin.Permissions = updatedAdmin.Permissions;
+
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Delete(int id)
+        {
+            var admin = admins.FirstOrDefault(a => a.Id == id);
+            if (admin == null) return NotFound();
+            return View(admin);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var admin = admins.FirstOrDefault(a => a.Id == id);
+            if (admin != null)
+            {
+                admins.Remove(admin);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ TeamWebShop/Models/DTOs/Admin/AdminDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamWebShop.Models.DTOs.Admins
+{
+    public class AdminDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = default!;
+
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; } = "Admin";
+
+        [Display(Name = "Permissions")]
+        public List<string>? Permissions { get; set; }
+    }
+}
