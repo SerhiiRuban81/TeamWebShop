e55649d81e7c9ea5be2d682ed75ec017d0110e54
KaterynaRe <katyareva88@gmail.com>
2025-04-21 13:10:41 +0300

    add home index

--- TeamWebShop/Controllers/HomeController.cs
+++ TeamWebShop/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeamWebShop.Data;
 
 namespace TeamWebShop.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ShopContext _context;
+
+        public HomeController(ShopContext context)
         {
-            return View();
+            this._context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products
+              .Include(c => c.Category)
+              .Include(b => b.Brand)
+              .Include(i=>i.ProductImages)
+              .ToListAsync();
+            
+            return View(products);
         }
     }
 }
